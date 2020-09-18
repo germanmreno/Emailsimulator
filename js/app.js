b2fea01dc876e6ffc86da5bc946c71190dcb90d4
Germán Moreno <grmmrno@gmail.com>
2020-09-18 18:41:49 -0400

    Reorganizando el proyecto

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const btnEnviar = document.querySelector("#enviar");
+const formulario = document.querySelector("#enviar-mail");
 
 const email = document.querySelector("#email");
 const asunto = document.querySelector("#asunto");
@@ -22,7 +23,51 @@ function iniciarApp() {
 function validarFormulario(e) {
   if (e.target.value.length > 0) {
     console.log("Hay algo");
+    mostrarExito(e.target);
   } else {
-    e.target.classList.add("border", "border-red-500");
+    mostrarError(e.target, "Todos los campos son obligatorios");
+  }
+
+  if (e.target.type === "email") {
+    const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (er.test(e.target.value)) {
+      console.log("Email válido");
+      mostrarExito(e.target);
+    } else {
+      mostrarError(e.target, "El email no es válido");
+    }
+  }
+}
+
+function mostrarExito(e) {
+  const error = document.querySelector("p.error");
+
+  error.remove();
+
+  e.classList.remove("border-red-500");
+  e.classList.add("border", "border-green-500");
+}
+
+function mostrarError(e, msj) {
+  e.classList.remove("border-green-500");
+  e.classList.add("border", "border-red-500");
+
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = msj;
+  mensajeError.classList.add(
+    "border",
+    "border-red-500",
+    "background-red-100",
+    "text-red-500",
+    "p-3",
+    "mt-5",
+    "text-center",
+    "error"
+  );
+
+  const errores = document.querySelectorAll(".error");
+  if (errores.length === 0) {
+    formulario.appendChild(mensajeError);
   }
 }
